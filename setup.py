98432a2ee1c555f567b0d96eb1a3b13a080406b4
Vladimir Filimonov <vladimir.a.filimonov@gmail.com>
2017-01-23 20:29:58 +0100

    Fix for wrong install_requires in setup.py (issue #1)

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ exec(open('co2meter/_version.py').read())
 
 # Long description to be published in PyPi
 LONG_DESCRIPTION = """
-**CO2meter** is a Python library for USB CO2 meter.
+**CO2meter** is a Python library for the USB CO2 meter.
 """
 
 setup(name='CO2meter',
       version=__version__,
       description='Python interface to the USB CO2 monitor',
+      long_description=LONG_DESCRIPTION,
       url=GITHUB_URL,
       download_url=GITHUB_URL + '/archive/v%s.zip' % (__version__),
       author='Vladimir Filimonov',
       author_email='vladimir.a.filimonov@gmail.com',
       license='MIT License',
       packages=['co2meter'],
-      install_requires=['hid']
+      install_requires=['hidapi']
       )
