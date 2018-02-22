1d73c24f75311a601741b60aca0e3428fd72cf69
Vladimir Filimonov <vladimir.a.filimonov@gmail.com>
2018-02-22 23:52:33 +0100

    zip_safe option 

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ exec(open('co2meter/_version.py').read())
 # Long description to be published in PyPi
 LONG_DESCRIPTION = """
 **CO2meter** is a Python interface to the USB CO2 monitor with monitoring and
-logging tools, flask/dash web-server for visualization and Apple HomeKit compatibility.
+logging tools, flask web-server for visualization and Apple HomeKit compatibility.
 """
 
 setup(name='CO2meter',
@@ -22,6 +22,7 @@ setup(name='CO2meter',
       packages=['co2meter'],
       install_requires=['hidapi', 'future'],
       include_package_data=True,
+      zip_safe=False,
       entry_points={
           'console_scripts': ['co2meter_server = co2meter:start_server',
                               'co2meter_homekit = co2meter:start_homekit',
